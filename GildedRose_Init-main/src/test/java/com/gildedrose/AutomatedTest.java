77100239ff95e3f0c01881f6fbe815e7addf8d47
rladkwjd <bada0369@naver.com>
2022-12-03 02:46:21 +0900

    COMMIT

--- GildedRose_Init-main/src/test/java/com/gildedrose/AutomatedTest.java
+++ GildedRose_Init-main/src/test/java/com/gildedrose/AutomatedTest.java
@@ -1,14 +1,15 @@
 package com.gildedrose;
 
-public class TexttestFixture {
+// TexttestFixture.java : non-automated golden-master test 변경
+public class AutomatedTest {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
@@ -18,20 +19,15 @@ public class TexttestFixture {
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
-        for (int i = 0; i < days; i++) {
+        for(int i=0; i<31; i++){
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (Item item : items) {
-                System.out.println(item);
+
+            for(int j=0; j<items.length; j++){
+                System.out.println(items[j]);
             }
-            System.out.println();
+            System.out.println("");
             app.updateQuality();
         }
     }
-
 }
